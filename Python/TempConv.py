1573751fa1d9df8a167acf998b7382d94986e8e4
Naveen Kumar R <naveenkumarr1309@gmail.com>
2025-09-01 10:14:28 +0530

    Test again

--- Python/TempConv.py
+++
@@ -1,4 +0,0 @@
-def ctf(c):
-    return (c*1.8)+32
-def ftc(f):
-    return (5/9)*(f-32)
--- Python/argv.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-print(len(sys.argv))
-tot=0
-print(sys.argv[0])
-for i in range(1,len(sys.argv)):
-    tot+=int(sys.argv[i])
-print(tot)
